f57055da4a6fc493333c55be9fa29dfab3d7cd3d
erics <erics@LAPTOP-PM2JJS50>
2019-07-31 01:09:29 -0500

    Reportes

--- src/ec/edu/ups/principal/Resportes.java
+++ src/ec/edu/ups/principal/Resportes.java
@@ -48,7 +48,7 @@ public class Resportes extends javax.swing.JInternalFrame {
                 .addComponent(jButton2)
                 .addGap(18, 18, 18)
                 .addComponent(jButton3)
-                .addContainerGap(255, Short.MAX_VALUE))
+                .addContainerGap(370, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -58,7 +58,7 @@ public class Resportes extends javax.swing.JInternalFrame {
                     .addComponent(jButton1)
                     .addComponent(jButton2)
                     .addComponent(jButton3))
-                .addContainerGap(241, Short.MAX_VALUE))
+                .addContainerGap(138, Short.MAX_VALUE))
         );
 
         pack();
